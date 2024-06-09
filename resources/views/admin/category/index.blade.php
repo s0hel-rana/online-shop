6a9008e663ae5f21043cd4adddb6bdb9bb50d5cb
s0hel-rana <sohelrananull@gmail.com>
2024-06-09 11:53:06 +0600

    list

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,45 +1,44 @@
 @extends('admin.master')
 @section('content')
-@push('style')
-    <!-- vendor css -->
-    <link href="{{ asset('backend') }}/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
-    <link href="{{ asset('backend') }}/lib/Ionicons/css/ionicons.css" rel="stylesheet">
-    <link href="{{ asset('backend') }}/lib/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
-    <link href="{{ asset('backend') }}/lib/highlightjs/github.css" rel="stylesheet">
-    <link href="{{ asset('backend') }}/lib/datatables/jquery.dataTables.css" rel="stylesheet">
-    <link href="{{ asset('backend') }}/lib/select2/css/select2.min.css" rel="stylesheet">
+    @push('style')
+        <!-- vendor css -->
+        <link href="{{ asset('backend') }}/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+        <link href="{{ asset('backend') }}/lib/Ionicons/css/ionicons.css" rel="stylesheet">
+        <link href="{{ asset('backend') }}/lib/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
+        <link href="{{ asset('backend') }}/lib/highlightjs/github.css" rel="stylesheet">
+        <link href="{{ asset('backend') }}/lib/datatables/jquery.dataTables.css" rel="stylesheet">
+        <link href="{{ asset('backend') }}/lib/select2/css/select2.min.css" rel="stylesheet">
 
-    <!-- Starlight CSS -->
-    <link rel="stylesheet" href="{{ asset('backend') }}/css/starlight.css">
-@endpush
-@php
-$links = [
-'Home'=>'',
-'Category list'=>''
-]
-@endphp
-<x-breadcrumb title='Category' :links="$links"/>
-    
-<div class="card pd-5 pd-sm-10">
-    <div class="card-body table-responsive">
-        <table id="datatable" class="table table-bordered table-hover">
-        </table>
-    </div><!-- table-wrapper -->
-</div><!-- card -->
+        <!-- Starlight CSS -->
+        {{-- <link rel="stylesheet" href="{{ asset('backend') }}/css/starlight.css"> --}}
+    @endpush
+    @php
+        $links = [
+            'Home' => '',
+            'Category list' => '',
+        ];
+    @endphp
+    <x-breadcrumb title='Category' :links="$links" />
+
+    <div class="card pd-5 pd-sm-10">
+        <div class="card-body table-responsive">
+            <table id="datatable" class="table table-bordered table-hover">
+            </table>
+        </div><!-- table-wrapper -->
+    </div><!-- card -->
 @endsection
 
 @push('script')
-    
     <script src="{{ asset('backend') }}/lib/jquery/jquery.js"></script>
     <script src="{{ asset('backend') }}/lib/popper.js/popper.js"></script>
-    <script src="{{ asset('backend') }}/lib/bootstrap/bootstrap.js"></script>
+    {{-- <script src="{{ asset('backend') }}/lib/bootstrap/bootstrap.js"></script> --}}
     <script src="{{ asset('backend') }}/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js"></script>
     <script src="{{ asset('backend') }}/lib/highlightjs/highlight.pack.js"></script>
     <script src="{{ asset('backend') }}/lib/datatables/jquery.dataTables.js"></script>
     <script src="{{ asset('backend') }}/lib/datatables-responsive/dataTables.responsive.js"></script>
     <script src="{{ asset('backend') }}/lib/select2/js/select2.min.js"></script>
 
-    <script src="{{ asset('backend') }}/js/starlight.js"></script>
+    {{-- <script src="{{ asset('backend') }}/js/starlight.js"></script> --}}
     <script type="text/javascript">
         $(function() {
             var table = $('#datatable').DataTable({
@@ -49,11 +48,11 @@ $links = [
                 processing: true,
                 ajax: "{{ route('categories.index') }}",
                 columns: [{
-                    data: "DT_RowIndex",
-                    title: "SL",
-                    name: "DT_RowIndex",
-                    searchable: false,
-                    orderable: false
+                        data: "DT_RowIndex",
+                        title: "SL",
+                        name: "DT_RowIndex",
+                        searchable: false,
+                        orderable: false
                     },
                     {
                         data: 'name',
@@ -78,5 +77,3 @@ $links = [
         });
     </script>
 @endpush
-
-
