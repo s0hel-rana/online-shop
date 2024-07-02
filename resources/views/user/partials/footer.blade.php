bcf74c8880b76ff9b2da00eb5b9b5a2a0fc49e2d
s0hel-rana <sohelrananull@gmail.com>
2024-07-02 16:58:19 +0600

    home page modify

---
+++ resources/views/user/partials/footer.blade.php
@@ -0,0 +1,51 @@
+
+<footer class="bg-dark mt-5">
+	<div class="container pb-5 pt-3">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="footer-card">
+					<h3>Get In Touch</h3>
+					<p>Welcome back to Our Online Shop. <br>
+					Nikunja-2, Dhaka-1229, Bangladesh <br>
+					sohelrananull@gmail.com <br>
+					01713616087</p>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="footer-card">
+					<h3>Important Links</h3>
+					<ul>
+						<li><a href="about-us.php" title="About">About</a></li>
+						<li><a href="contact-us.php" title="Contact Us">Contact Us</a></li>						
+						<li><a href="#" title="Privacy">Privacy</a></li>
+						<li><a href="#" title="Privacy">Terms & Conditions</a></li>
+						<li><a href="#" title="Privacy">Refund Policy</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="footer-card">
+					<h3>My Account</h3>
+					<ul>
+						<li><a href="#" title="Sell">Login</a></li>
+						<li><a href="#" title="Advertise">Register</a></li>
+						<li><a href="#" title="Contact Us">My Orders</a></li>						
+					</ul>
+				</div>
+			</div>			
+		</div>
+	</div>
+	<div class="copyright-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 mt-3">
+					<div class="copy-right text-center">
+						<p>© Copyright {{ date('Y') }} Online Shop. All Rights Reserved</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
